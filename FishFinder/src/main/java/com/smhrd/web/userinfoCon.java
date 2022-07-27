eac7c6c78a82f31308b0543320392f90171005c9
smhrd <smhrd@121.147.52.236>
2022-07-27 17:50:22 +0900

    url 매핑 겹친부분 수정

--- FishFinder/src/main/java/com/smhrd/web/userinfoCon.java
+++ FishFinder/src/main/java/com/smhrd/web/userinfoCon.java
@@ -165,47 +165,6 @@ public class userinfoCon {
 	}
 	
 	
-	@PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile file,@RequestParam("file2") MultipartFile file2, Model model) {
-		
-		if(file.getOriginalFilename().equals("")) {
-			file = file2;
-			System.out.println("if 파일 이름 : " + file.getOriginalFilename());
-		}
- 
-		int user_num = (Integer)session.getAttribute("user_num");
-		String access_Token = (String)session.getAttribute("access_Token");
-		
-        System.out.println("파일 이름 : " + file.getOriginalFilename());
-        System.out.println("파일 크기 : " + file.getSize());
-        System.out.println("사용자 번호 : " + user_num);
-
-     // 실제 물리경로
-        String reaPath = "C:\\Users\\smhrd\\git\\FishFinder\\FishFinder\\src\\main\\webapp\\resources\\image\\"; 
-        
-        LocalDate now = LocalDate.now();
-        String fileName = now + "_" +file.getOriginalFilename();
-        
-        // file 경로와 유저 정보를 DB에 저장
-        userhistory vo = is.DBUpdate(fileName, user_num);
-
-        // File을 서버에 저장
-        is.ServerUpdate(file, reaPath, fileName);
-        // 프로젝트에 업로드된 이미지 프로젝트에 바로 적용하는 방법이 있는 주소
-        // 안하면 바로 업데이트 안돼서 기능 안댐
-        //https://record-than-remember.tistory.com/entry/%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%ED%8C%8C%EC%9D%BC-%EC%97%85%EB%A1%9C%EB%93%9C-%ED%9B%84%EC%97%90-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%9E%90%EB%8F%99-refresh
-        
-        // 플라스크 서버 통신 (이미지 경로, 사용자 토큰)
-        //int his_seq_res = 
-        is.Flask(vo.getFish_img(), access_Token, vo.getHis_seq());
-        
-        //위 Flask 함수에서 일어날 일들이 다 이뤄지고 다음 줄로 넘어가면 좋겠는데 
-        
-        
-        return "redirect:/result/"+vo.getHis_seq();
-    }
-	
-	
 	
 	// AI분석 후, 히스토리에서 접근
 	@RequestMapping("/result/{his_seq}")
